6153c130175a0256ff8717c25b3b7edf29226645
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-06-04 11:50:17 +0530

    Basic Setup

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x) cout << #x << " : " << x << endl;
+#define MOD 1000000007
+
+#define toDigit(a) a - '0';
+#define toChar(num) '0' + num;
+
+#define endl "\n"
+#define PIE 3.141592653
+
+int main() {
+    // Main Function
+    return 0;
+};
